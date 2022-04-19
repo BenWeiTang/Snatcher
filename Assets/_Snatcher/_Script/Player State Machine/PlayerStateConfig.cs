6c892a9456566abc1288b864411baf3714383ef9
Ben Tang <sjps30503@gmail.com>
2022-04-19 16:54:58 -0400

    4/19 start dev

--- Assets/_Snatcher/_Script/Player State Machine/PlayerStateConfig.cs
+++ Assets/_Snatcher/_Script/Player State Machine/PlayerStateConfig.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Snatcher
@@ -6,16 +7,26 @@ namespace Snatcher
     public class PlayerStateConfig : ScriptableObject
     {
         [Header("Locomotion")]
-        public float MoveSpeed;
-        public float TurnSpeed;
+        public float MoveSpeed = 7.5f;
+        public float TurnSpeed = 15f;
 
         [Header("Gravity")]
-        public float GroundedGravity = -0.05f;
+        public float GroundedGravity = -0.5f;
         public float AirborneGravity = -9.8f;
         [Min(0f)]
-        public float MaxFallSpeed = 100f;
+        public float MaxFallSpeed = 50f;
         
         [Header("Visual")]
         public GameObject Limb;
+
+        private void Reset()
+        {
+            MoveSpeed = 7.5f;
+            TurnSpeed = 15f;
+            GroundedGravity = -0.5f;
+            AirborneGravity = -9.8f;
+            MaxFallSpeed = 50f;
+            Limb = null;
+        }
     }
 }
