001c3aa208d50aa6c7c162872a8596089a49000e
Ben Tang <sjps30503@gmail.com>
2022-04-19 19:09:13 -0400

    Created Hook In State

---
+++ Assets/_Snatcher/_Script/Player State Machine/Concrete States/Basic/PlayerBasicHookInState.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Snatcher
+{
+    public class PlayerBasicHookInState : APlayerBasicState
+    {
+        public PlayerBasicHookInState(PlayerStateMachine currentContext, PlayerStateFactory currentFactory) : base(currentContext, currentFactory)
+        {
+        }
+
+        public override void EnterState(bool hasSameSuperState)
+        {
+            base.EnterState(hasSameSuperState);
+        }
+
+        public override void ExitState()
+        {
+        }
+
+        public override void UpdateState()
+        {
+        }
+
+        protected override void CheckSwitchState()
+        {
+        }
+    }
+}
