9e571e2d04e618760e12a20e296293dd7e9adfeb
Ben Tang <sjps30503@gmail.com>
2022-04-19 19:09:23 -0400

    Created Hook Out State

---
+++ Assets/_Snatcher/_Script/Player State Machine/Concrete States/Basic/PlayerBasicHookOutState.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Snatcher
+{
+    public class PlayerBasicHookOutState : APlayerBasicState
+    {
+        public PlayerBasicHookOutState(PlayerStateMachine currentContext, PlayerStateFactory currentFactory) : base(currentContext, currentFactory) { }
+
+        public override void EnterState(bool hasSameSuperState)
+        {
+        }
+
+        public override void ExitState() { }
+
+        public override void UpdateState() { }
+
+        protected override void CheckSwitchState() { }
+    }
+}
