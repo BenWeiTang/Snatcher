94f8a08b8d0f1cac4556f4ade46c94cb4c1fe0ae
Ben Tang <sjps30503@gmail.com>
2022-04-20 10:35:50 -0400

    Update HookController.cs
    - Show hit box

--- Assets/_Snatcher/_Script/Player State Machine/HookController.cs
+++ Assets/_Snatcher/_Script/Player State Machine/HookController.cs
@@ -9,8 +9,21 @@ namespace Snatcher
         //TODO: we want to determine if the timing is right and we can snatch the ability
         public event Action OnEnemyHit;
         [SerializeField] private BoxCollider _collider;
+        
+        // Remove later; for debug use only
+        private MeshRenderer _meshRenderer;
+        
+        public void ActivateCollider(bool toActivate)
+        {
+            _meshRenderer.enabled = toActivate;
+            _collider.enabled = toActivate;
+        }
 
-        public void ActivateCollider(bool toActivate) => _collider.enabled = toActivate;
+        private void Awake()
+        {
+            // Remove later; for debug use only
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
 
         private void OnTriggerEnter(Collider other)
         {
