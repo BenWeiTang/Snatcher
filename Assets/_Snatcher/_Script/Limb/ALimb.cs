2eff034e37d433511b953e9b279d00fb9b2eecae
adamburich <burichadam@gmail.com>
2022-04-23 17:01:19 -0400

    basic

--- Assets/_Snatcher/_Script/Limb/ALimb.cs
+++ Assets/_Snatcher/_Script/Limb/ALimb.cs
@@ -3,9 +3,13 @@ namespace Snatcher
     public abstract class ALimb
     {
         public abstract LimbType Type { get; }
+        private string name { get; }
+        private AUpgrade upgrade { get; set; }
+        private float durability { get; set; }
         
-        private string name;
-        private AUpgrade[] upgrades;
-        private float durability;
+        public ALimb()
+        {
+        }
+
     }
 }
