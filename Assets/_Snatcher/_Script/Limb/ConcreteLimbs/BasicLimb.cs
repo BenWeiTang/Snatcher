5492ebe0d004311c2899efceea48d2ba0f6de212
Ben Tang <sjps30503@gmail.com>
2022-04-23 17:05:15 -0400

    Rename Basic.cs to BasicLimb.cs

--- Assets/_Snatcher/_Script/Limb/ConcreteLimbs/BasicLimb.cs
+++ Assets/_Snatcher/_Script/Limb/ConcreteLimbs/BasicLimb.cs
@@ -1,15 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 namespace Snatcher
 {
-    public class Basic : ALimb
+    public class BasicLimb : ALimb
     {
         public override LimbType Type => LimbType.Basic;
         private string name = "basic";
         private float durability = float.PositiveInfinity;
-        public Basic() : base()
+        public BasicLimb() : base()
         {
         }
     }
