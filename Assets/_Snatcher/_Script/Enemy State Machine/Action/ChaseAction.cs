a7344c76917069d4ef4c3af770b568d1f1dc863b
Ben Tang <sjps30503@gmail.com>
2022-05-02 09:22:29 -0400

    Removed unused using directives

--- Assets/_Snatcher/_Script/Enemy State Machine/Action/ChaseAction.cs
+++ Assets/_Snatcher/_Script/Enemy State Machine/Action/ChaseAction.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Web.Mvc;
 using UnityEngine;
 
 namespace Snatcher
--- Assets/_Snatcher/_Script/Enemy State Machine/Concrete State/GuardState.cs
+++ Assets/_Snatcher/_Script/Enemy State Machine/Concrete State/GuardState.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.Remoting.Contexts;
 using UnityEngine;
 
 namespace Snatcher
