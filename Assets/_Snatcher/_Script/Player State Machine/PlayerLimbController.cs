5173e033fbfe8c7593b6275600b0ec2b187bb30a
adamburich <burichadam@gmail.com>
2022-05-10 20:15:42 -0400

    revert

--- Assets/_Snatcher/_Script/Player State Machine/PlayerLimbController.cs
+++ Assets/_Snatcher/_Script/Player State Machine/PlayerLimbController.cs
@@ -5,13 +5,18 @@ namespace Snatcher
 {
     public class PlayerLimbController : MonoBehaviour
     {
-        [SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
+        //[SerializeField] private SkinnedMeshRenderer _skinnedMeshRenderer;
         [SerializeField] private VoidEvent _onLimbSwitched;
         [SerializeField] private Mesh _oneArmMesh;
-        [SerializeField] private Mesh _twoArmMesh;
+        //[SerializeField] private Mesh _twoArmMesh;
+
+        [SerializeField] private GameObject _propellerLimb;
+        [SerializeField] private GameObject _legLimb;
 
         private void OnEnable()
         {
+            _propellerLimb.SetActive(false);
+            _legLimb.SetActive(false);
             _onLimbSwitched.RegisterListener(OnLimbSwitched);
         }
         
@@ -20,16 +25,28 @@ namespace Snatcher
             _onLimbSwitched.UnregisterListener(OnLimbSwitched);
         }
         
-        //TODO: delete later
         private void OnLimbSwitched(Void _)
         {
+            _propellerLimb.SetActive(false);
+            _legLimb.SetActive(false);
+
             if (LimbManager.Instance.CurrentLimb.Type == LimbType.Basic)
             {
-                _skinnedMeshRenderer.sharedMesh = _oneArmMesh;
+                _propellerLimb.SetActive(false);
+                _legLimb.SetActive(false);
+                //_skinnedMeshRenderer.sharedMesh = _oneArmMesh;
+            }
+            else if (LimbManager.Instance.CurrentLimb.Type == LimbType.Leg)
+            {
+                _legLimb.SetActive(true);
+                //_skinnedMeshRenderer.sharedMesh = _propellerLimb;
+                //_skinnedMeshRenderer.BakeMesh(_propellerLimb);
             }
             else if (LimbManager.Instance.CurrentLimb.Type == LimbType.Invis)
             {
-                _skinnedMeshRenderer.sharedMesh = _twoArmMesh;
+                _propellerLimb.SetActive(true);
+                //_skinnedMeshRenderer.sharedMesh = _legLimb;
+                //_skinnedMeshRenderer.BakeMesh(_legLimb);
             }
         }
     }
