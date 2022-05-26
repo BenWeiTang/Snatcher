30a9cc99ee3330d2b8c3a178c0e8f2325d01de86
adamburich <burichadam@gmail.com>
2022-05-25 21:14:17 -0400

    works

--- Assets/_Snatcher/_Script/Limb/ConcreteLimbs/BasicLimb.cs
+++ Assets/_Snatcher/_Script/Limb/ConcreteLimbs/BasicLimb.cs
@@ -7,7 +7,7 @@ namespace Snatcher
     {
         public sealed override GameObject Model => Resources.Load<GameObject>($"Limb/{GetType().ToString().Substring(9)}");
         public override LimbType Type => LimbType.Basic;
-        public override string Name => "Basic";
+        public override string Name => "Hook";
         public override ASuperState SuperState => PlayerStateFactoryManager.Instance.BasicState;
         public sealed override List<AUpgrade> Upgrades { get; protected set; }
         //public override float Durability { get; protected set; } = 100f;
